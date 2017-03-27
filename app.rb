1659a923a3ff31f9d2d1d5128652d41be97945b5
kittysquee <kittysquee@gmail.com>
2017-03-27 19:59:23 +0100

    Creates views directory

--- app.rb
+++ app.rb
@@ -5,24 +5,6 @@ get '/' do
   'hello!'
 end
 
-get '/secret' do
-  'Will ❤️\'s Big Bittiner'
-end
-
-get '/root' do
-  'Big Toby'
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'
-  </div>"
-end
-
- get '/sausages' do
-   'pork'
- end
-
-get '/willisshit' do
-  '💩💩💩'
+  erb(:index)
 end
